cbe255c7cc71c26df59fa0eedb9fc40ae5838464
Valentina <118310524+valeamayola@users.noreply.github.com>
2024-04-08 20:16:54 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 export default function Home() {
   return (
     <>
-      <div className="h-max md:h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-zinc-950 to-zink-800">
+      <div className="h-max md:h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-zinc-900 to-zink-900">
         <NavBar />
         <Header />
       </div>
