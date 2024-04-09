64bfa09bbe510cfc099b532b747a7ab6e099e0bb
Valentina <118310524+valeamayola@users.noreply.github.com>
2024-04-09 17:57:49 -0300

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,14 +2,14 @@ export default function About() {
     return (
         <section id="about" className="bg-[#EAE7E4] p-10 md:p-20 items-start">
             <h1 className="text-4xl md:text-7xl font-extrabold">Hola,</h1>
-            <p className="font-medium text-lg md:text-xl mt-5">
+            <p className="font-medium text-lg md:text-3xl mt-5">
                 Mi nombre es Valentina, soy desarrolladora front-end apasionada por el diseño y el desarrollo web.
                 Disfruto usando mi creatividad y mis habilidades para crear interfaces intuitivas, atractivas y fáciles de usar.
                 Me gradué de la Universidad Santo Tomás de Aquino como Técnica en Desarrollo y Calidad de Software.
                 Actualmente estoy estudiando Diseño UX / UI en la Universidad Tecnológica Nacional de Buenos Aires.
                 Siempre estoy buscando nuevos desafíos y oportunidades para aprender y crecer.
             </p>
-            <p className="font-medium text-lg md:text-xl mt-5">
+            <p className="font-medium text-lg md:text-3xl mt-5">
                 Bienvenid@ a mi portafolio, ¡espero que te guste! 
             </p>
         </section>
